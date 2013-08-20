5193ff97b82330a638363ca18c1b3fa574225550
ytnobody <ytnobody@gmail.com>
2013-08-20 17:34:57 +0900

    add docs

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 # NAME
 
-WWW::MetXML - It's new $module
+WWW::MetXML - Perl Interface of MetXML(MetBroker XML)
 
 # SYNOPSIS
 
     use WWW::MetXML;
+    my $metxml = WWW::MetXML->new;
+    my $res = $metxml->get(
+        lang       => 'ja',
+        source     => 'amedas', 
+        station    => '40336', 
+        begin      => '2013-08-19', 
+        end        => '2013-08-20', 
+        resolution => 'hourly',
+        elements   => [qw/airtemperature rain wind/],
+    );
+    my $temp = $res->element('airtemperature');
+    my $max_temp = $res->subelement('Max');
+    say $max_temp->value('2013-08-19 13:00'); # 33.6 (unit = Celsius temp.)
+    my $wind = $res->element('wind');
+    my $wind_speed = $res->subelement('Speed');
+    say $wind_speed->value('2013-08-19 13:00'); # 3 (unit = m/s)
 
 # DESCRIPTION
 
-WWW::MetXML is ...
+MetXML(MetBroker XML) is an open API for accessing stored weather data, by National Agriculture and Food Research Organization, of Japan.
+
+WWW::MetXML provides object oriented interface for MetXML.
 
 # LICENSE
 
@@ -20,3 +38,7 @@ it under the same terms as Perl itself.
 # AUTHOR
 
 ytnobody <ytnobody@gmail.com>
+
+# SEE ALSO
+
+[http://pc105.narc.affrc.go.jp/metbroker/xml/](http://pc105.narc.affrc.go.jp/metbroker/xml/)
--- lib/WWW/MetXML.pm
+++ lib/WWW/MetXML.pm
@@ -14,15 +14,33 @@ __END__
 
 =head1 NAME
 
-WWW::MetXML - It's new $module
+WWW::MetXML - Perl Interface of MetXML(MetBroker XML)
 
 =head1 SYNOPSIS
 
     use WWW::MetXML;
+    my $metxml = WWW::MetXML->new;
+    my $res = $metxml->get(
+        lang       => 'ja',
+        source     => 'amedas', 
+        station    => '40336', 
+        begin      => '2013-08-19', 
+        end        => '2013-08-20', 
+        resolution => 'hourly',
+        elements   => [qw/airtemperature rain wind/],
+    );
+    my $temp = $res->element('airtemperature');
+    my $max_temp = $res->subelement('Max');
+    say $max_temp->value('2013-08-19 13:00'); # 33.6 (unit = Celsius temp.)
+    my $wind = $res->element('wind');
+    my $wind_speed = $res->subelement('Speed');
+    say $wind_speed->value('2013-08-19 13:00'); # 3 (unit = m/s)
 
 =head1 DESCRIPTION
 
-WWW::MetXML is ...
+MetXML(MetBroker XML) is an open API for accessing stored weather data, by National Agriculture and Food Research Organization, of Japan.
+
+WWW::MetXML provides object oriented interface for MetXML.
 
 =head1 LICENSE
 
@@ -34,6 +52,10 @@ it under the same terms as Perl itself.
 =head1 AUTHOR
 
 ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+L<http://pc105.narc.affrc.go.jp/metbroker/xml/>
 
 =cut
 
